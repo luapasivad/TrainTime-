233508bb91ae03939aec2f35b16995a3924fdf6a
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-04-23 15:42:58 -0400

    basic site layout

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,40 +1,12 @@
  // Initialize Firebase
  var config = {
-    apiKey: "AIzaSyBJTrA8rBVBlyIcwejzkd5xAMQMvi9M8u4",
-    authDomain: "timesheet-9fbf9.firebaseapp.com",
-    databaseURL: "https://timesheet-9fbf9.firebaseio.com",
-    projectId: "timesheet-9fbf9",
-    storageBucket: "timesheet-9fbf9.appspot.com",
-    messagingSenderId: "886091612748"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBF536Nb6zi0Oty-LggWoa43NvjOf_ud-w",
+  authDomain: "traintime-58706.firebaseapp.com",
+  databaseURL: "https://traintime-58706.firebaseio.com",
+  projectId: "traintime-58706",
+  storageBucket: "traintime-58706.appspot.com",
+  messagingSenderId: "457133562984"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database()
-
-// Initial values (subjected to change)
-var firstName = ""
-var lastName = ""
-var role = ""
-var start = 0
-var rate = ""
-
-$("#").on("click", function(event) {
-    event.preventDefault()
-
-    // Grabbing value from text box 
-    firstName = $("#first-name").val().trim()
-    lastName = $("#last-name").val().trim()
-    role = $("#role").val().trim()
-    start = $("#start-date").val().trim()
-    rate = $("#monthly-rate").val().trim()
-
-    database.ref().push({
-        firstName: firstName,
-        lastName: lastName,
-        role: role,
-        start: start,
-        rate: rate
-    })
-
-    console.log(lastName)
-})
